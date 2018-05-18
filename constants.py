9a169c2ad481b535be27182dbcd5e09a07d5dccf
Mikhail Volynov <st3althtech@mail.ru>
2018-05-18 13:28:09 +0300

    Added project configuration file

--- constants.py
+++ constants.py
@@ -1,10 +1,17 @@
 import os
+import configparser
 
-COMMAND_EXECUTOR = 'http://127.0.0.1:4444/wd/hub'
+SETTINGS_FILEPATH = 'conf/app.conf'
+SETTINGS_SECTION = 'SETTINGS'
+
+config = configparser.ConfigParser()
+config.read_file(open(SETTINGS_FILEPATH))
+
+COMMAND_EXECUTOR = config.get(SETTINGS_SECTION, 'command_executor')
 DIRECTORY = os.path.dirname(os.path.abspath(__file__))
-SCREENSHOT_PATH = os.path.join(DIRECTORY, 'screenshots')
-WAIT_TIME = 60
-LONG_WAIT_TIME = 100
-MAKE_SCREENSHOTS = False
-BASE_URL = 'https://ok.ru/'
-SKIP_FINISHED_TESTS = True
+SCREENSHOT_PATH = os.path.join(DIRECTORY, config.get(SETTINGS_SECTION, 'screenshot_dir'))
+WAIT_TIME = config.getint(SETTINGS_SECTION, 'wait_time')
+LONG_WAIT_TIME = config.getint(SETTINGS_SECTION, 'long_wait_time')
+MAKE_SCREENSHOTS = config.getboolean(SETTINGS_SECTION, 'make_screenshots')
+BASE_URL = config.get(SETTINGS_SECTION, 'base_url')
+SKIP_FINISHED_TESTS = config.getboolean(SETTINGS_SECTION, 'skip_finished_tests')
